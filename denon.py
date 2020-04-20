2e308ae00c0c2b18758d19eeb22435e9a916a79e
Heckie75 <github@heckie.de>
2020-04-20 18:54:06 +0200

    Migrated script to python 3

--- denon.py
+++ denon.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 #
 # MIT License
 #
@@ -55,284 +55,286 @@ __STATS = "statics"
 __PARAMS = "params"
 __PARSER = "parser"
 
+__PARAM = -1
+
 COMMANDS = {
     "on" : {
          __USAGE : "on",
          __DESCR : "Turns denon receiver on",
-         __STATS : "\x01\x02\x00"
+         __STATS : [ 1, 2, 0 ]
          },
      "off" : {
          __USAGE : "off",
          __DESCR : "Turns denon receiver off",
-         __STATS : "\x02\x01\x00"
+         __STATS : [ 2, 1, 0 ]
          },
      "fm" : {
          __USAGE : "fm",
          __DESCR : "Sets input source to FM radio",
-         __STATS : "\x10\x00\x00"
+         __STATS : [ 16, 0, 0 ]
          },
      "dab" : { # can't test
          __USAGE : "dab",
          __DESCR : "Sets input source to DAB radio",
-         __STATS : "\x12\x00\x00"
+         __STATS : [ 18, 0, 0 ]
          },
      "cd" : {
          __USAGE : "cd",
          __DESCR : "Sets input source to CD (digital-in for CD)",
-         __STATS : "\x13\x00\x00"
+         __STATS : [ 19, 0, 0 ]
          },
      "net" : {
          __USAGE : "net",
          __DESCR : "Sets input source to Network (digital-in NETWORK)",
-         __STATS : "\x14\x00\x00"
+         __STATS : [ 20, 0, 0 ]
          },
      "analog" : {
          __USAGE : "analog <1|2>",
          __DESCR : "Sets input source to Analog n, where n is 1 or 2",
-         __STATS : "%s\x00\x00",
+         __STATS : [ __PARAM, 0, 0 ],
          __PARAMS : [{
-             "1" : "\x15",
-             "2" : "\x16"
+             "1" : 21,
+             "2" : 22
              }]
          },
      "optical" : {
          __USAGE : "optical",
          __DESCR : "Sets input source to Digital-In optical",
-         __STATS : "\x17\x00\x00"
+         __STATS : [ 23, 0, 0 ]
          },
      "mode" : {
          __USAGE : "mode",
          __DESCR : "Toggles stereo/mono mode",
-         __STATS : "\x28\x00\x00"
+         __STATS : [ 40, 0, 0 ]
          },
      "play" : { # can't test
          __USAGE : "play",
          __DESCR : "Starts playback",
-         __STATS : "\x32\x00\x00"
+         __STATS : [ 50, 0, 0 ]
          },
      "pause" : { # can't test
          __USAGE : "pause",
          __DESCR : "Pauses current playback",
-         __STATS : "\x32\x00\x00"
+         __STATS : [ 50, 0, 0 ]
          },
      "stop" : { # can't test
          __USAGE : "stop",
          __DESCR : "Stops current playback",
-         __STATS : "\x33\x00\x00"
+         __STATS : [ 51, 0, 0 ]
          },
      "vol" : {
          __USAGE : "vol <0-60>",
          __DESCR : "Sets volume to value whish is between 0 and 60",
-         __STATS : "\x40\x00%s",
-         __PARAMS : [range(0x3c)]
+         __STATS : [ 64, 0, __PARAM ],
+         __PARAMS : [ range(60) ]
          },
      "mute" : {
          __USAGE : "mute <on|off>",
          __DESCR : "Mute on/off",
-         __STATS : "\x41\x00%s",
+         __STATS : [ 65, 0, __PARAM ],
          __PARAMS : [{
-             "on" : "\x01",
-             "off" : "\x00"
+             "on" : 1,
+             "off" : 0
              }]
          },
      "sdb" : {
          __USAGE : "sdb <on|off>",
          __DESCR : "SDB sound option on/off",
-         __STATS : "\x42\x00\x00%s",
+         __STATS : [ 66, 0, 0, __PARAM ],
          __PARAMS : [{
-             "on" : "\x00",
-             "off" : "\x01"
+             "on" : 0,
+             "off" : 1
              }]
          },
      "bass" : {
          __USAGE : "bass <+|->",
          __DESCR : "Increases / decreases bass level",
-         __STATS : "\x42\x00\x01%s",
+         __STATS : [ 66, 0, 1, __PARAM ],
          __PARAMS : [{
-             "+" : "\x00",
-             "-" : "\x01"
+             "+" : 0,
+             "-" : 1
              }]
          },
      "treble" : {
          __USAGE : "treble <+|->",
          __DESCR : "Increases / decreases treble level",
-         __STATS : "\x42\x00\x02%s",
+         __STATS : [ 66, 0, 2, __PARAM ],
          __PARAMS : [{
-             "+" : "\x00",
-             "-" : "\x01"
+             "+" : 0,
+             "-" : 1
              }]
          },
      "balance" : {
          __USAGE : "balance <left|right>",
          __DESCR : "Sets balance one step more to left or right",
-         __STATS : "\x42\x00\x03%s",
+         __STATS : [ 66, 0, 3, __PARAM ],
          __PARAMS : [{
-             "left" : "\x00",
-             "right" : "\x01"
+             "left" : 0,
+             "right" : 1
              }]
          },
      "sdirect" : {
          __USAGE : "sdirect <on|off>",
          __DESCR : "Activates/deactivates s.direct input",
-         __STATS : "\x42\x00\x04%s",
+         __STATS : [ 66, 0, 4, __PARAM ],
          __PARAMS : [{
-             "on" : "\x00",
-             "off" : "\x01"
+             "on" : 0,
+             "off" : 1
              }]
          },
      "dimmer" : {
          __USAGE : "dimmer <high|normal|low|off>",
          __DESCR : "Sets brightness of display",
-         __STATS : "\x43\x00%s",
+         __STATS : [ 67, 0, __PARAM ],
          __PARAMS : [{
-             "high" : "\x00",
-             "normal" : "\x01",
-             "low" : "\x02",
-             "off" : "\x03"
+             "high" : 0,
+             "normal" : 1,
+             "low" : 2,
+             "off" : 3
              }]
          },
      "next" : { # can't test
          __USAGE : "next",
          __DESCR : "Jump to next title",
-         __STATS : "\x44\x00\x00"
+         __STATS : [ 68, 0, 0 ]
          },
      "previous" : { # can't test
          __USAGE : "previous",
          __DESCR : "Jump to previous title",
-         __STATS : "\x45\x00\x00"
+         __STATS : [ 69, 0, 0 ]
          },
      "forward" : { # can't test
          __USAGE : "forward",
          __DESCR : "Forwards in current title",
-         __STATS : "\x46\x00\x00"
+         __STATS : [ 70, 0, 0 ]
          },
      "rewind" : { # can't test
          __USAGE : "rewind",
          __DESCR : "Rewinds in current title",
-         __STATS : "\x47\x00\x00"
+         __STATS : [ 71, 0, 0 ]
          },
      "up" : {
          __USAGE : "up",
          __DESCR : "Moves in current menu up",
-         __STATS : "\x48\x00\x00"
+         __STATS : [ 72, 0, 0 ]
          },
      "down" : {
          __USAGE : "down",
          __DESCR : "Moves in current menu down",
-         __STATS : "\x49\x00\x00"
+         __STATS : [ 73, 0, 0 ]
          },
      "left" : {
          __USAGE : "left",
          __DESCR : "Moves in current menu to the left",
-         __STATS : "\x4a\x00\x00"
+         __STATS : [ 74, 0, 0 ]
          },
      "right" : {
          __USAGE : "right",
          __DESCR : "Moves in current menu to the right",
-         __STATS : "\x4b\x00\x00"
+         __STATS : [ 75, 0, 0 ]
          },
      "enter" : {
          __USAGE : "enter",
          __DESCR : "Commit current setting by pressing enter",
-         __STATS : "\x4c\x00\x00"
+         __STATS : [ 76, 0, 0 ]
          },
      "search" : {
          __USAGE : "search",
          __DESCR : "Enter search menu",
-         __STATS : "\x4d\x00\x00"
+         __STATS : [ 77, 0, 0 ]
          },
      "num" : {
          __USAGE : "num <0-9|+10>",
          __DESCR : "Sends numeric buttom 0-9 / '+10' to receiver",
-         __STATS : "%s\x00\x00",
+         __STATS : [ __PARAM, 0, 0 ],
          __PARAMS : [{
-             "1" : "\x4f",
-             "2" : "\x50",
-             "3" : "\x51",
-             "4" : "\x52",
-             "5" : "\x53",
-             "6" : "\x54",
-             "7" : "\x55",
-             "8" : "\x56",
-             "9" : "\x57",
-             "0" : "\x58",
-             "+10" : "\x59"
+             "1" : 79,
+             "2" : 80,
+             "3" : 81,
+             "4" : 82,
+             "5" : 83,
+             "6" : 84,
+             "7" : 85,
+             "8" : 86,
+             "9" : 87,
+             "0" : 88,
+             "+10" : 89
              }]
          },
      "clear" : {
          __USAGE : "clear",
          __DESCR : "Sends clear command",
-         __STATS : "\x5a\x00\x00"
+         __STATS : [ 90, 0, 0 ]
          },
      "random" : { # can't test
          __USAGE : "random",
          __DESCR : "(does not seem to work)",
-         __STATS : "\x5c\x00\x00"
+         __STATS : [ 92, 0, 0 ]
          },
      "repeat" : { # can't test
          __USAGE : "repeat",
          __DESCR : "Toggles repeat option for playback",
-         __STATS : "\x5d\x00\x00"
+         __STATS : [ 93, 0, 0 ]
          },
      "info" : {
          __USAGE : "info",
          __DESCR : "Toggles display",
-         __STATS : "\x5e\x00\x00"
+         __STATS : [ 94, 0, 0 ]
          },
      "cda" : {
          __USAGE : "cda",
          __DESCR : "Sets input source to CD and selects CD",
-         __STATS : "\x5f\x00\x00"
+         __STATS : [ 95, 0, 0 ]
          },
      "usb" : {
          __USAGE : "usb",
          __DESCR : "Sets input source to CD and selects USB",
-         __STATS : "\x60\x00\x00"
+         __STATS : [ 96, 0, 0 ]
          },
      "online" : {
          __USAGE : "online",
          __DESCR : "Sets input source to Network "
             + "and selects online music",
-         __STATS : "\x61\x00\x00"
+         __STATS : [ 97, 0, 0 ]
          },
      "internet" : {
          __USAGE : "internet",
          __DESCR : "Sets input source to Network "
             + "and selects internet radio",
-         __STATS : "\x62\x00\x00"
+         __STATS : [ 98, 0, 0 ]
          },
      "server" : {
          __USAGE : "server",
          __DESCR : "Sets input source to Network and selects server",
-         __STATS : "\x63\x00\x00"
+         __STATS : [ 98, 0, 0 ]
          },
      "ipod" : {
          __USAGE : "ipod",
          __DESCR : "Sets input source to Network and selects iPod",
-         __STATS : "\x63\x00\x00"
+         __STATS : [ 98, 0, 0 ]
          },
      "preset" : {
          __USAGE : "preset <+|->",
          __DESCR : "Zaps to previous / next preset",
-         __STATS : "\x68\x30%s",
+         __STATS : [ 104, 48, __PARAM ],
          __PARAMS : [{
-             "+" : "\x00",
-             "-" : "\x01"
+             "+" : 0,
+             "-" : 1
              }]
          },
      "sleep" : {
          __USAGE : "sleep <0-255>",
          __DESCR : "Activates sleep mode with time in minutes",
-         __STATS : "\x6b\x00%s",
-         __PARAMS : [range(256)]
+         __STATS : [ 107, 0, __PARAM ],
+         __PARAMS : [ range(256) ]
          },
      "standby" : {
          __USAGE : "standby <on|off>",
          __DESCR : "Sets auto-standby on/off",
-         __STATS : "\x83\x00%s",
+         __STATS : [ 131, 0, __PARAM ],
          __PARAMS : [{
-             "off" : "\x00",
-             "on" : "\x01"
+             "off" : 0,
+             "on" : 1
              }]
          },
      "set-alarm" : {
@@ -341,7 +343,7 @@ COMMANDS = {
             + "[<preset no.>]",
          __DESCR : "Configures alarm clock, e.g. set-alarm once "
             + "21:17 23:45 preset24",
-         __STATS : "%s\x00\x00%s\x00%s%s",
+         __STATS : [ __PARAM, 0, 0, __PARAM, __PARAM, 0, __PARAM, __PARAM, __PARAM ],
          __PARSER : [None,
                      [__PARSE_VAL, __PARSE_VAL],
                      [__PARSE_VAL, __PARSE_VAL],
@@ -350,8 +352,8 @@ COMMANDS = {
                       __PARSE_IDX, __PARSE_IDX, __PARSE_IDX,
                       __PARSE_VAL]],
          __PARAMS : [{
-                "once" : "\x88",
-                "everyday" : "\x89",
+                "once" : 136,
+                "everyday" : 137,
              },
              r"([0-2]?[0-9]):([0-5][0-9])",
              r"([0-2]?[0-9]):([0-5][0-9])",
@@ -361,12 +363,12 @@ COMMANDS = {
      "alarm" : {
          __USAGE : "alarm <off|on|once|everyday>",
          __DESCR : "Activates / deactivates alarm timers",
-         __STATS : "\x8a\x00%s",
+         __STATS : [ 138, 0, __PARAM ],
          __PARAMS : [{
-             "off" : "\x00",
-             "everyday" : "\x01",
-             "once" : "\x02",
-             "on" : "\x03"
+             "off" : 0,
+             "everyday" : 1,
+             "once" : 2,
+             "on" : 3
              }]
         },
      "wait" : {
@@ -485,7 +487,7 @@ def build_binary_commands_from_rc(rc_commands):
             rc_seq += " " + rc_key
 
             # handle parameter of type list (range of int values)
-            if type(param_def) in (tuple, list):
+            if type(param_def) in (tuple, list, range):
                 params.append(__interprete_param_array(cmd_def,
                                                 rc_key,
                                                 param_def))
@@ -498,18 +500,34 @@ def build_binary_commands_from_rc(rc_commands):
 
             # handle parameter of keywords (lookedup by regexp)
             elif type(param_def) in (tuple, str):
-                params.append(__interprete_param_regex(cmd_def,
+                params += __interprete_param_regex(cmd_def,
                                                 rc_key,
                                                 param_def,
-                                                cmd_def[__PARSER][i]))
+                                                cmd_def[__PARSER][i])
 
         # collect commands
         binary_commands.append({
-                "binary" : raw_seq % tuple(params),
+                "binary" : __replace_params(raw_seq, params),
                 "rc_cmd" : rc_seq
             })
 
     return binary_commands
+
+
+
+
+def __replace_params(raw_seq, params):
+
+    params.reverse()
+
+    rv = []
+    for b in raw_seq:
+        if ( b == -1):
+            rv += [ params.pop() ]
+        else:
+            rv += [ b ]
+
+    return rv
 
 
 
@@ -535,7 +553,7 @@ def __interprete_param_array(cmd_def, cli_arg, cmd_param_def):
                    + "> is out of allowed range:"))
 
     # return int value of cli_arg as char
-    return chr(int(cli_arg))
+    return int(cli_arg)
 
 
 
@@ -567,28 +585,28 @@ def __interprete_param_regex(cmd_def, cli_arg, cmd_param_def, parser):
     if matcher == None:
         raise ex
 
-    s = __parse(matcher, parser)
-    if s == "":
+    b = __parse(matcher, parser)
+    if len(b) == 0:
         raise ex
 
-    return s
+    return b
 
 
 
 
 def __parse(matcher, instruc):
 
-    s = ""
+    b = []
     for i in range(len(instruc)):
         m = matcher.group(i +1)
         if m == None:
             continue
         elif instruc[i] == __PARSE_VAL :
-            s += chr(int(matcher.group(i + 1)))
+            b += [ int(matcher.group(i + 1)) ]
         elif instruc[i] == __PARSE_IDX:
-            s += chr(i)
+            b += [ i ]
 
-    return s
+    return b
 
 
 
@@ -645,13 +663,16 @@ def __init_serial():
         print(" INFO: Force serial device to <" + port + ">")
         dev = port
 
-    ser = serial.Serial(dev, baudrate=115200, 
-                        bytesize=serial.EIGHTBITS,
-                        parity=serial.PARITY_NONE, 
-                        stopbits=serial.STOPBITS_ONE, 
-                        timeout=1, 
-                        rtscts=True, 
-                        dsrdtr=True)
+    ser = serial.Serial(dev, baudrate = 115200, 
+                        bytesize = serial.EIGHTBITS,
+                        parity = serial.PARITY_NONE, 
+                        stopbits = serial.STOPBITS_ONE, 
+                        timeout = 5, 
+                        write_timeout = 5,
+                        rtscts = True, 
+                        dsrdtr = True,
+                        xonxoff = False,
+                        exclusive = True)
 
 
 
@@ -669,14 +690,14 @@ def __close_serial():
 def __build_package(data):
 
     # preample
-    package = "\xFF\x55"
+    package = [ 255, 85 ]
 
     # length
     length = len(data) - 2
-    package += chr(length)
+    package += [ length ]
 
     # static
-    package += "\x01\x00"
+    package += [ 1, 0 ]
 
     # data
     package += data
@@ -684,10 +705,10 @@ def __build_package(data):
     # checksum
     checksum = 0
     for i in package:
-        checksum += ord(i)
+        checksum += i
 
     checksum %= 256
-    package += chr(checksum)
+    package += [ checksum ]
 
     return package
 
@@ -869,5 +890,5 @@ if __name__ == "__main__":
             sendto_denon(commands)
 
     except HelpException as e:
-        print e.message
+        print(e.message)
         exit(1)
